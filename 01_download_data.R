c23a8212e112cdbb1ae9af6a904334b40e8b3e57
lwawrowski <lukasz8989@gmail.com>
2019-09-04 13:04:05 +0200

    downloaded data

--- 01_download_data.R
+++ 01_download_data.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(jsonlite)
 library(lubridate)
 
-api_key <- ""
+api_key <- "e1b299f31e2142b3b05793f58d2f368d"
 
 get_news <- function(api_key){
   
@@ -11,23 +11,37 @@ get_news <- function(api_key){
   d <- data.frame()
   
   for(i in 1:length(category)){
-    
-    di <- fromJSON(paste0("https://newsapi.org/v2/top-headlines?country=pl&pageSize=100&category=",category[i],"&apiKey=",api_key))
-    
-    di_df <- cbind(di$articles$source,di$articles[,2:ncol(di$articles)]) %>% mutate(category=category[i])
-    
-    d <- rbind(d, di_df)
+
+    out <- tryCatch({
+      
+      url <- paste0("https://newsapi.org/v2/top-headlines?country=pl&pageSize=100&category=",category[i],"&apiKey=",api_key)
+      
+      di <- fromJSON(url)
+      
+      di_df <- cbind(di$articles$source,di$articles[,2:ncol(di$articles)]) %>% mutate(category=category[i])
+      
+      d <- rbind(d, di_df)
+      
+    }, error = function(e){
+      
+      message(e)
+      
+      return(d)
+      
+    })
     
   }
   
-  return(d)
+  return(out)
 
 }
 
+a <- Sys.time()
 news <- get_news(api_key)
+Sys.time() - a
 
-start_time <- "14-08-2019 16:00"
-end_time <- "29-08-2019 16:00"
+start_time <- "18-08-2019 23:00"
+end_time <- "2-09-2019 3:00"
 
 int <- dmy_hm(start_time) %--% dmy_hm(end_time)
 time<- int@.Data/60
@@ -44,14 +58,14 @@ for(t in 1:n){
   
   news_all <- rbind(news_all, news)
   
-  if(t %% 30 == 0){
+  if(t %% 34 == 0){
     save(news_all, file = paste0("data/dane_",format(Sys.time(),"%d_%m_%Y_%H_%M"),".RData"))
     
     news_all <- data.frame()
   }
   
   if(t != n){
-    Sys.sleep(3540)  
+    Sys.sleep(3590)  
   }
   
 }
---
+++ 02_union_data.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+pliki <- list.files("data")
+
+news <- data.frame()
+
+for(i in 1:length(pliki)){
+  
+  load(paste0("data/",pliki[i]))
+  
+  news <- union_all(news, news_all)
+  
+}
+
+googlenews <- news %>%
+  distinct()
+
+# news 137557 obs.
+# googlenews 19805 obs.
+# 14% unique
+
+save(googlenews, file="googlenews.RData")
